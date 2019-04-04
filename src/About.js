91c8cf0db2fa725101e84963d5702680a2e404c3
Narmuzz <narmuzz95@gmail.com>
2019-04-04 17:57:48 -0300

    listo, falta boton numerico.

--- src/About.js
+++ src/About.js
@@ -4,18 +4,11 @@ import "./App.css";
 function About() {
   return (
     <article className="article">
-      <h2 className="article-title">About Weeb Ranker</h2>
+      <h2 className="article-title">About the Weeb Ranker</h2>
       <p className="article-text">
-        The Weeb Ranke lalalalallalalal WIP. Lorem ipsum dolor sit amet,
-        consectetur adipiscing elit. Aenean fermentum, diam eu tempus fringilla,
-        ligula urna consequat ligula, sit amet laoreet sapien quam eu diam.
-        Proin venenatis porttitor quam iaculis mollis. Nam a tellus tempor,
-        ultricies sapien quis, scelerisque metus. Nulla ut lectus purus.
-        Vestibulum eleifend purus urna, eget malesuada ligula finibus eu. Ut
-        interdum sem nunc, sit amet porttitor quam vehicula ut. Proin porttitor
-        libero at nisl scelerisque finibus. Proin eget bibendum erat. Nunc quis
-        hendrerit mauris, a sollicitudin odio. Etiam sit amet purus interdum,
-        lobortis ex vitae, dapibus est. Sed tincidunt quis leo non pellentesque.
+        The Weeb Ranker will carefully analize your taste using the data
+        available through your myanimelist account to provide you with an
+        estimation of how you are as a weeb.
       </p>
     </article>
   );
--- src/WeebRanker.js
+++ src/WeebRanker.js
@@ -1,14 +1,67 @@
 import React, { Component } from "react";
 
-class WeebRanker extends Component {
-  state = { value: "", meanScore: null, loading: false };
+function Rank(props) {
+  if (!props.meanScore) {
+    return null;
+  }
+  const limits = [3, 5, 6, 7, 8, 10];
+  const texts = [
+    {
+      title: "You're Hayao Miyazaki",
+      content:
+        "You simply hate anime. You feel disgusted by it mere existance. In your heart you know that anime was a mistake."
+    },
+    {
+      title: "You don't like chinese cartoons",
+      content: `If someone saying "why would anyone still be watching cartoons if they are more than 12 years old?" sounds a lot like you, that's no coincidence.`
+    },
+    {
+      title: "You compare everything to Evangelion",
+      content:
+        "At some point in your life you watched the show to beat al shows, the quintessential artwork of anime. Ever since, nothing stands as tall and proud as Evangelion does. All anime is infirior to it and as such it should be compared an held in submission to it."
+    },
+    {
+      title: "You are the average weeb",
+      content:
+        "You are the representative of the countless hordes of anime fans around the world, the mirror of their taste and mind. You are the best example of what most weebs are like, at least regarding to taste."
+    },
+    {
+      title: "You love harem/romcom anime",
+      content:
+        "Romance is your drug. You live to see your ships come to fruition, to see the kiss of the season happen. Hand holding is the reason of your existence."
+    },
+    {
+      title: "You are Kirito",
+      content:
+        "Your taste knows no bounds. You are anime. You are all that is good. You are the waifu magnet."
+    }
+  ];
+  let result = "";
+  for (let i = 0; i < limits.length; i++) {
+    if (props.meanScore <= limits[i]) {
+      result = texts[i];
+      break;
+    }
+  }
+  return (
+    <article className="article">
+      <h2 className="article-title">{result.title}</h2>
+      <p className="article-text">{result.content}</p>
+    </article>
+  );
+}
+
+class UserInput extends Component {
+  state = {
+    loading: false,
+    value: ""
+  };
+
   handleChange = event => {
     this.setState({
       value: event.target.value
     });
   };
-
-  ///for y array para loopear y que se yo. Tomar el mean score y comprarlo con el array de 6 posiciones, ver cuaderno.
 
   handleKeyDown = event => {
     if (event.key === "Enter") {
@@ -25,14 +78,17 @@ class WeebRanker extends Component {
     );
     const json = await response.json();
     this.setState({
-      meanScore: json.anime_stats.mean_score,
       loading: false
     });
+    this.props.onChangeMeanScore(json.anime_stats.mean_score);
   };
+
   render() {
     return (
-      <div>
+      <>
+        Enter a Myanimelist account
         <input
+          className="input"
           disabled={this.state.loading}
           value={this.state.value}
           onChange={this.handleChange}
@@ -41,7 +97,32 @@ class WeebRanker extends Component {
         <button disabled={this.state.loading} onClick={this.handleClick}>
           Send
         </button>
-        {this.state.meanScore}
+      </>
+    );
+  }
+}
+
+class NumberInput extends Component {
+  state = {
+    loading: false,
+    value: ""
+  };
+}
+
+class WeebRanker extends Component {
+  state = { meanScore: null };
+
+  changeMeanScore = newMeanScore => {
+    this.setState({
+      meanScore: newMeanScore
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <UserInput onChangeMeanScore={this.changeMeanScore} />
+        <Rank meanScore={this.state.meanScore} />
       </div>
     );
   }
